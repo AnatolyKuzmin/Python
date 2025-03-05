9d51c653b5ab62c2c10aa5b77539aedde7aa68e3
Anatolii Kuzmin <kuzinaa8@yandex.ru>
2025-03-05 22:40:53 +0300

    GET, POST

--- 3_Веб-разработка_(Flask_Django)/my_flask_app/app.py
+++ 3_Веб-разработка_(Flask_Django)/my_flask_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 # Создаем экземпляр приложения Flask
 app = Flask(__name__)
@@ -12,6 +12,20 @@ def home():
 @app.route("/user/<name>")
 def greet_user(name):
     return f"Привет, {name}!"
+
+# Обработка различных HTTP-методов
+@app.route("/form", methods=["GET", "POST"])
+def form():
+    if request.method == "POST":
+        name = request.form.get("name")
+        return f"Привет, {name} (POST)!"
+    return """
+        <form method="post">
+            <label for="name">Имя:<label>
+            <input type="text" id="name" name="name">
+            <button type="submit">Отправить</button>
+        </form>
+    """
 
 # Запускаем приложение
 if __name__ == "__main__":
