4d11a2902430a23d4ae697d9491a26be5ef201f0
Anatolii Kuzmin <kuzinaa8@yandex.ru>
2025-03-06 22:04:04 +0300

    Jinja2

--- 3_Веб-разработка_(Flask_Django)/my_flask_app/app.py
+++ 3_Веб-разработка_(Flask_Django)/my_flask_app/app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template("index.html", name="Иван")
 
+@app.route("/users")
+def users():
+    users_list = [
+        {"name": "Иван", "age": 25},
+        {"name": "Мария", "age": 30},
+        {"name": "Петр", "age": 35}
+    ]
+    return render_template("users.html", users=users_list)
+
 if __name__ == "__main__":
     app.run(debug=True)
